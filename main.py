4deefa6bfa531b1e607597efa7e6ee6b7e1eafeb
Lucas <lucasgvargas96@gmail.com>
2021-05-11 20:25:27 -0300

    first cleanup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 import requests
 from dotenv import load_dotenv
@@ -15,44 +16,19 @@ projects = ['crm', 'folclass', 'webpj']
 states = ['start', 'stop']
 rolesProjects = ['CRM', 'Folclass', 'Web PJ']
 client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    # print('We have logged in as {0.user}'.format(client))
+    game = discord.Game('Developing the API')
+    await bot.change_presence(status=discord.Status.idle, activity=game)
+    print('My Ready is Body')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$instance') and message.content != '$instance help':
-        print(client.user.avatar_url)
-        text = message.content.split()
-        project = text[1]
-        state = text[2]
-        role_names = [role.name for role in message.author.roles]
-        if project not in projects or state not in states:
-            await message.channel.send(f'Ocurrio un problema, revisa que los argumentos sean correctos')
-        else:
-            roleCheck = True if 'test' in role_names or 'Folclass' in role_names or 'Web PJ' in role_names else False
-            if ('test' in role_names) and roleCheck:
-                response = requests.post(f'{jenkinsUrl}/job/instance-manager/buildWithParameters?token={jenkinsToken}&PROJECT={project}&STATE={state}', auth=(authName, authToken))
-                if response:
-                    statusInfo = 'Iniciando' if state == 'start' else 'Parando'
-                    await message.channel.send(f'{statusInfo} la instancia de {project}')
-                else:
-                    await message.channel.send(f'Ocurrio un problema')
-            else:
-                await message.channel.send(f'No tenes los permisos suficientes para ejecutar este comando')
-                
-    if message.content.startswith('$instance help'):
-        embed=discord.Embed(title='Instance Manager Help', description='mensaje de ayuda del bot Instance Manager', color=0x68cf7e)
-        embed.set_author(name=client.user.display_name, url='', icon_url=client.user.avatar_url)
-        embed.set_thumbnail(url=client.user.avatar_url)
-        embed.add_field(name='$instance project state', value='Este comando se utiliza para gestionar el estasdo de las instancias de desarrollo, se debe reemplazar _project_  por **crm**, **folclass** o **webpj** \n y _state_ por **start** o **stop**', inline=False)
-        embed.add_field(name= 'ejemplo', value='**$instance folclass start** \n _esto incia la instancia de desarollo del proyecto Folclass_', inline=False)
-        # embed.set_footer(text=time.ctime(time.time()))
-        await message.channel.send(embed=embed)
-        
-client.run(token)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
+
+bot.run(token)
