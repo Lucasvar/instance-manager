891b2798ee7215bf4f9c30238b428a815012000e
Lucas <lucasgvargas96@gmail.com>
2021-05-13 23:47:06 -0300

    small test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import requests
 from dotenv import load_dotenv
 import time
+import boto3
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN_DEV')
@@ -23,12 +24,15 @@ async def on_ready():
     # print('We have logged in as {0.user}'.format(client))
     game = discord.Game('Developing the API')
     await bot.change_presence(status=discord.Status.idle, activity=game)
-    print('My Ready is Body')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+async def instance(ctx):
+    await ctx.send('Instance command!')
 
 
 bot.run(token)
